2c0ba71788253f88d893272f9cbceeb190cb8b26
Adrien Albuquerque <adrialb95@gmail.com>
2022-04-04 16:57:55 +0200

    finish

--- MeteoCloud/my-app/src/components/App.tsx
+++ MeteoCloud/my-app/src/components/App.tsx
@@ -2,15 +2,23 @@ import React from 'react';
 import '../CSS/App.css';
 import SearchForm from './SearchForm';
 import ShowMeteo from './ShowMeteo';
+import FetchData from './FetchData';
+import {useEffect} from "react";
+import {useState} from "react";
 
 function App() {
+  const [inputForm  , setinputForm] = useState<string >('');
+  const [meteo, setMeteo] = useState<any[any]>([]);
+
+
   return (
       <div className='container'>
         <div className='form'>
-          <SearchForm />
+          <SearchForm inputForm = {inputForm} setMeteo= {setMeteo}  setinputForm={setinputForm}/>
         </div>
+        
         <div className='text-row'>
-          <ShowMeteo />
+          <ShowMeteo meteo = {meteo}/>
         </div>
       </div>
   );
--- MeteoCloud/my-app/src/components/ShowMeteo.tsx
+++ MeteoCloud/my-app/src/components/ShowMeteo.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
 
-function ShowMeteo() {
+function ShowMeteo( props: { meteo: any[any] } ) {
+    console.log(props.meteo)
   return (
-      <span>Non</span>
+      <div>
+          {props.meteo.hasOwnProperty('current') && 
+            <div>
+                <h1>{props.meteo.location.name} {props.meteo.location.country}</h1>
+                <h1>{props.meteo.current.temp_c}</h1>
+                
+            </div>  }
+          
+      </div>
   );
 }
 
